659169f53dbf5efef48fdd2b9ea0b86b24887256
mudgen <nick@perfectabstractions.com>
2021-09-22 17:13:24 -0400

    Removed unused contracts

--- contracts/MegaPoolFactory.sol
+++
@@ -1,8 +0,0 @@
-
-// Author: Nick Mudge (nick@perfectabstractions.com, @mudgen, https://twitter.com/mudgen)
-
-// SPDX-License-Identifier: MIT
-
-//https://github.com/pancakeswap/pancake-farm/blob/master/contracts/MasterChef.sol
-
-
